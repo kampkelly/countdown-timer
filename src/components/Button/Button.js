e47acf741efdfa6c8ca7e6263011334427e833eb
Runor Adjekpiyede <kampkellykeys@gmail.com>
2020-06-13 13:59:03 +0100

    make countdown interactive

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,16 +4,18 @@ import PropTypes from "prop-types";
 import './Button.scss';
 
 const Button = (props) => {
+
   return (
     <div className="button">
-      <button className={props.selected ? "selected" : ""}>{props.text}</button>
+      <button className={props.selected ? "selected" : ""} onClick={() => props.onClick()}>{props.text}</button>
     </div>
   )
 };
 
 Button.propTypes = {
-    selected: PropTypes.string,
-    text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  selected: PropTypes.string,
+  text: PropTypes.string.isRequired,
 };
 
 export default Button;
--- src/components/CountDownTimer/TimeInput/TimeInput.js
+++ src/components/CountDownTimer/TimeInput/TimeInput.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
+import { CountdownContext } from "../../../state/Contexts/CountdownContext";
 import Button from '../../Button/Button';
 import './TimeInput.scss';
 
 const TimeInput = () => {
+  const [time, setTime] = useState('');
+  const [error, setError] = useState(false);
+  const { state, setState } = useContext(CountdownContext);
+  const { toolTipText } = state;
+  
+  const validInput = () => {
+    // validate time input
+    if (!time || time <= 0 || time > 60 || !Number.isInteger(Number(time))) {
+      console.log('enter')
+      setError(true);
+      setState({ ...state, toolTipText: 'input is invalid' });
+      return false;
+    }
+    return true;
+  };
+
+  const handleInputChange = (e) => {
+    setError(false);
+    setState({ ...state, toolTipText: '' });
+    setTime(e.target.value);
+  };
+
+  const onStart = () => {
+    if (validInput()) {
+      const roundedTime = Math.round(time);
+      setState({
+        ...state,
+        duration: roundedTime,
+        infoText: '',
+        minutes: roundedTime,
+        seconds: 0,
+        pause: true,
+        resume: false,
+      });
+    }
+  };
+
   return (
     <div className="time-input">
       <div><span>Countdown:</span></div>
-      <div><input type="number" placeholder="(Min)"></input> </div>
-      <Button text="START" />
+      <div className="input-container"><input type="number" className={error ? "error" : ""} placeholder="(Min)"
+       onChange={handleInputChange} value={time}></input>
+       <span className="tooltip">{toolTipText}</span> </div>
+      <Button onClick={onStart} text="START" />
     </div>
   )
 };
